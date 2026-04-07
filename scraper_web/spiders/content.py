f1d02b8e6a3620400b0a307316e481bc0fcbed06
shaggy <pawlaczykblazej@gmail.com>
2026-04-07 20:55:22 +0200

    Meta data

--- scraper_web/spiders/content.py
+++ scraper_web/spiders/content.py
@@ -10,13 +10,28 @@ class Content(scrapy.Spider):
         self.start_urls = [url]
 
     def parse(self, response):
+        meta = self.meta_data(response)
         images = self.extract_images(response)
 
         yield {
             'image_urls': images,
             'page': response.url,
-            'total_images': len(images)
+            'total_images': len(images),
+            'meta_data': meta
         }
+
+    #Ekstrakcja Title
+
+    #Ekstrakcja Meta danych
+    def meta_data(self, response):
+        return {
+            'title': response.css('title::text').get(),
+            'description': response.css('meta[name="description"]::attr(content)').get(),
+            'keywords': response.css('meta[name="keywords"]::attr(content)').get(),
+            'og_title': response.css('meta[property="og:title"]::attr(content)').get(),
+        }
+
+    #Eksrakcja Tekstu
 
     #Ekstrakcja Obrazow
     def extract_images(self, response):
