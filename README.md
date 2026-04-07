477abdf369bf2f2b924f039115f44373080c221d
shaggy <pawlaczykblazej@gmail.com>
2026-04-07 20:41:25 +0200

    Podzielenie kodu na czesci

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Scraper Web
 
-Projekt Scrapy do web scrapingu.
-
+Prosty program scraper + open ai do tworzenie profili firm na ppodstawie istniejacych stron interentowych
 ## Instalacja
 
 ```bash
@@ -16,38 +15,12 @@ pip install scrapy
 ## Uruchomienie
 
 ```bash
-# Lista dostępnych spider'ów
-scrapy list
-
-# Uruchomienie spider'a
-scrapy crawl cytaty_spider
-
 # Zapis do pliku
 scrapy crawl cytaty_spider -o output.json
 scrapy crawl cytaty_spider -o output.csv
 ```
 
-## Struktura projektu
 
-```
-scraper_web/
-├── scrapy.cfg              # Konfiguracja Scrapy
-├── scraper_web/
-│   ├── __init__.py
-│   ├── items.py           # Definicje items
-│   ├── middlewares.py     # Custom middlewares
-│   ├── pipelines.py       # Data pipelines
-│   ├── settings.py        # Ustawienia projektu
-│   └── spiders/           # Spider'y
-│       ├── __init__.py
-│       └── cytaty_spider.py
-```
-
-## Tworzenie nowego spider'a
-
-```bash
-scrapy genspider nazwa_spider example.com
-```
 ## Korzystanie z programu
 ```
 scrapy crawl content -a url=https://quotes.toscrape.com/page/2/
--- scraper_web/spiders/content.py
+++ scraper_web/spiders/content.py
@@ -3,25 +3,26 @@ import scrapy
 
 class Content(scrapy.Spider):
     name = 'content'
-    
+
+    #Dynamiczne wybieranie url
     def __init__(self, url=None, *args, **kwargs):
         super(Content, self).__init__(*args, **kwargs)
-        
-        if url:
-            self.start_urls = [url]
-        else:
-            # Domyślny URL jeśli nie podano
-            print("Link ERROR")
-    
+        self.start_urls = [url]
+
     def parse(self, response):
+        images = self.extract_images(response)
+
+        yield {
+            'image_urls': images,
+            'page': response.url,
+            'total_images': len(images)
+        }
+
+    #Ekstrakcja Obrazow
+    def extract_images(self, response):
         # Zbierz wszystkie URLe obrazów
         image_urls = response.css('img::attr(src)').getall()
-        
+
         # Konwertuj relatywne URLe na absolutne
         image_urls = [response.urljoin(url) for url in image_urls]
-        
-        yield {
-            'image_urls': image_urls,
-            'page': response.url,
-            'total_images': len(image_urls)
-        }
+        return image_urls
